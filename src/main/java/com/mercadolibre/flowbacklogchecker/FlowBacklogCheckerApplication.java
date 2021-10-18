24ad35a0f0085089bc7123f698238fd23c3c3b7b
guspollitzer <gustavo.pollitzer@mercadolibre.com>
2021-10-18 12:32:39 -0300

    Build the project skeleton

---
+++ src/main/java/com/mercadolibre/flowbacklogchecker/FlowBacklogCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.flowbacklogchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowBacklogCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowBacklogCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercadolibre/flowbacklogchecker/FlowBacklogCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.flowbacklogchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowBacklogCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
